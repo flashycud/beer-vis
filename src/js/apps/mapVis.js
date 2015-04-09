afbc231f355f65efd360f73852282f4f5e37db18
Flash <me@flashycud.com>
2015-04-08 20:40:22 -0400

    attached state name to Topo json

--- src/js/apps/mapVis.js
+++ src/js/apps/mapVis.js
@@ -8,7 +8,9 @@ define([
 
   var _options = {
     width: 960,
-    height: 500
+    height: 500,
+
+    color_range: [d3.rgb(29,145,145), d3.rgb(196,196,196)]
 
   }
   
@@ -22,22 +24,24 @@ define([
 
     var path = d3.geo.path();
 
+    var states;
+
     d3.json("js/json/us.json", function(error, topology) {
-      svg.append("path")
-          .datum(topojson.feature(topology, topology.objects.land))
-          .attr("d", path)
-          .attr("class", "land-boundary");
+    // d3.json("us.json", function(error, topology) {
 
-      // svg.append("path")
-      //     .datum(topojson.mesh(topology, topology.objects.counties, function(a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); }))
-      //     .attr("d", path)
-      //     .attr("class", "county-boundary");
-
-      svg.append("path")
-          .datum(topojson.mesh(topology, topology.objects.states, function(a, b) { return a !== b; }))
+      states = svg.selectAll("path")
+          .data(topojson.feature(topology, topology.objects.states).features)
+        .enter().append("path")
           .attr("d", path)
           .attr("class", "state-boundary");
+      console.log(topojson.feature(topology, topology.objects.states))
     });
+
+    var color = setColorScale(options.color_range)
+  }
+
+  function setColorScale(color_range) {
+    return d3.scale.linear().range(color_range);
   }
 
   return MapVis;
