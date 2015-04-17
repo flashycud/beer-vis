9e8b2d17e51f7df9ea1ce1f1033ca650bc06202b
David Cheng <davidcheng817@gmail.com>
2015-04-17 03:33:47 -0400

    new upload

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,18 @@
 requirejs.config({
   baseUrl: 'js',
+  shim: {
+        d3: {
+          exports: 'd3'
+        },
+        radar: {
+          exports: 'radar',
+          deps: ['d3']
+        },
+        wordcloud: {
+          exports: 'wordcloud',
+          deps: ['d3']
+        }
+  },
   paths: {
     app: 'main',
     d3: 'lib/d3/d3',
@@ -9,6 +22,9 @@ requirejs.config({
     underscore: 'lib/underscore/underscore',
     text: 'lib/requirejs-text/text',
     json: 'lib/requirejs-plugins/src/json',
+    d3tip: 'lib/d3-tip/index',
+    radar: 'lib/radar/radar-chart',
+    wordcloud: 'lib/wordcloud/d3.layout.cloud'
   }
 });
 
--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,11 @@ define([
   'd3',
   'jquery',
   'underscore',
-  // './apps/dataHandler',
-  // './apps/parallelCoordinates'
-  './apps/mapVis'
-
+  './apps/mapVis',
+  './apps/treeVis',
+  './apps/detailVis'
 ],
-function(d3, $, _, /*DataHandler, ParallelCoordinates,*/ MapVis ) {
+function(d3, $, _, MapVis, TreeVis, DetailVis) {
 
   var cur_style_lvl = 1,
       cur_style = '',
@@ -18,6 +17,9 @@ function(d3, $, _, /*DataHandler, ParallelCoordinates,*/ MapVis ) {
     color_range: [d3.rgb(204,226,228),d3.rgb(0,139 ,149)]
   });
   mapVis.registerUpdate(update);
+  TreeVis(update);
+  
+  var updateDetailVis = DetailVis;
 
   var wordVis;
 
@@ -25,15 +27,17 @@ function(d3, $, _, /*DataHandler, ParallelCoordinates,*/ MapVis ) {
   
   $('#style').change(function(e){
     mapVis.redrawMap($(this).val());
+    update();
   });
-
 
   function update(){
     cur_state = mapVis.getSelectedState();
 
-    wordVis.updateWordCloud(cur_style_lvl, cur_style, cur_state);
-    barVis.updateBars(cur_style_lvl, cur_style, cur_state);
+    // wordVis.updateWordCloud(cur_style_lvl, cur_style, cur_state);
+    // barVis.updateBars(cur_style_lvl, cur_style, cur_state);
     mapVis.updateMap(cur_style);
+    updateDetailVis(cur_style_lvl, cur_style, cur_state);
 
   }
+
 });
